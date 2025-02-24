587d837210467b4c5ac9942c2f06a62ecbf6dd42
ant385525 <ant385525@yahoo.com>
2025-02-24 12:08:15 -0800

    fix(fileflows) latest version of app not container

--- apps/fileflows/fileflows-server/ci/latest.sh
+++ apps/fileflows/fileflows-server/ci/latest.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-IMAGE_NAME="revenz/fileflows"
-API_ENDPOINT="https://hub.docker.com/v2/repositories/${IMAGE_NAME}/tags/?page=1&page_size=100&ordering=last_updated"
-response=$(curl -s ${API_ENDPOINT})
+ENDPOINT="https://fileflows.com/auto-update/latest-version?platform=docker"
+response=$(curl -s ${ENDPOINT})
 
-latest_version=$(echo "${response}" | jq -r '.results[] | select(.name | match("^[0-9]+.[0-9]+$")) | .name' | head -n 1)
-
-digest=$(echo "${response}" | jq ".results[] | select(.name == \"${latest_version}\") | .digest" | tr -d '"' )
-
-printf "Latest Version: %s\nCommit Hash: %s\n" "${latest_version}" "${digest}"
+printf ${latest_version}
