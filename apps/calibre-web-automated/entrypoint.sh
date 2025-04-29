26f116c085cf5652e085708d1bafbcb43c101a23
ant385525 <ant385525@yahoo.com>
2025-04-29 09:45:52 -0700

    clean up tests, add handling for multiple process exiting

--- apps/calibre-web-automated/entrypoint.sh
+++ apps/calibre-web-automated/entrypoint.sh
@@ -13,14 +13,24 @@ bash /init-scripts/cwa-auto-library.sh
 # Fully initialize db to set custom settings
 bash /init-scripts/cwa-db-config.sh
 
+pids=()
 echo "init scripts complete. Starting services in the background..."
 # https://github.com/crocodilestick/Calibre-Web-Automated/blob/main/root/etc/s6-overlay/s6-rc.d/cwa-auto-zipper/run
-bash /init-scripts/cwa-auto-zipper.sh &
+bash /init-scripts/cwa-auto-zipper.sh & pids+=($!)
 # https://github.com/crocodilestick/Calibre-Web-Automated/blob/main/root/etc/s6-overlay/s6-rc.d/cwa-ingest-service/run
-bash /init-scripts/cwa-ingest-service.sh &
+bash /init-scripts/cwa-ingest-service.sh & pids+=($!)
 # https://github.com/crocodilestick/Calibre-Web-Automated/blob/main/root/etc/s6-overlay/s6-rc.d/metadata-change-detector/run
-bash /init-scripts/cwa-metadata-service.sh &
+bash /init-scripts/cwa-metadata-service.sh & pids+=($!)
 
 echo "services started. Starting app..."
 # start app
-exec python3 /app/calibre-web/cps.py
+python3 /app/calibre-web/cps.py & pids+=($!)
+
+# Wait until any process exits
+wait -n "${pids[@]}"
+exit_code=$?
+exit $exit_code
+
+echo "A process died with code ${exit_code} shutting container down..."
+kill "${pids[@]}" 2>/dev/null || true
+exit $exit_code
