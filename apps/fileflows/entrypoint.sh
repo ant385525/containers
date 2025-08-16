e41afd0a31d1b75330885cb55f8ca5490d5c7b8a
Anthony Clerici <anthony@clerici.me>
2025-08-16 10:50:15 -0700

    fix(fileflows) try no --docker, add base-dir flag

--- apps/fileflows/entrypoint.sh
+++ apps/fileflows/entrypoint.sh
@@ -8,7 +8,7 @@ cp -r /var/tmp/i18n/* /app/Server/wwwroot/i18n/
 if [ "$FFNODE" = "0" ]; then
     echo "Starting Fileflows Server..."
     cd /app/Server
-    exec dotnet FileFlows.Server.dll --urls="http://0.0.0.0:${PORT};http://[::]:${PORT}" --docker
+    exec dotnet FileFlows.Server.dll --urls="http://0.0.0.0:${PORT};http://[::]:${PORT}" --base-dir="${BASE_DIR}
 elif [ "$FFNODE" = "1" ]; then
     if [ -z "$ServerUrl" ]; then
         echo "Error: Running as a node but 'ServerUrl' environment variable is not set."
@@ -17,7 +17,7 @@ elif [ "$FFNODE" = "1" ]; then
 
     echo "Starting FileFlows Node..."
     cd /app/Node
-    exec dotnet FileFlows.Node.dll --docker
+    exec dotnet FileFlows.Node.dll --base-dir="${BASE_DIR}
 else
     echo "Unknown FFNODE value: '$FFNODE'"
     echo "To run the node set the FFNODE env to '1'"
